52cc03fa710e26f61cf335573332b1399bb04297
Alberto Jose <albertojcvs@gmail.com>
2021-12-23 09:39:03 -0300

    add the middleaware isAdmin

---
+++ Bank_Sytem/app/Middleware/IsAdmin.ts
@@ -0,0 +1,21 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class IsAdmin {
+  public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
+    const id = await auth.user?.id
+
+    if (id) {
+      const user = await User.findOrFail(id)
+
+      await user.load('permissions')
+      const hasAdminPermission = user.permissions.some((permission) => permission.name === 'admin')
+
+      if (hasAdminPermission) await next()
+      else
+        return response
+          .status(403)
+          .send({ error: { message: 'Only authenticated admins can use this route' } })
+    }
+  }
+}
