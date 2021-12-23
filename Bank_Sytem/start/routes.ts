e271b57681da3b6df5a9b89649cdcf6d7d523eae
Alberto Jose <albertojcvs@gmail.com>
2021-12-23 09:38:43 -0300

    add the users and permissions routes

--- Bank_Sytem/start/routes.ts
+++ Bank_Sytem/start/routes.ts
@@ -1,25 +1,22 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
+Route.group(() => {
+  Route.post('/users', 'UsersController.store')
+
+  Route.group(() => {
+    Route.get('/users/:id', 'UsersController.show')
+    Route.get('/users', 'UsersController.index')
+    Route.put('/users', 'UsersController.update')
+    Route.delete('/users', 'UsersController.destroy')
+  }).middleware('auth:api')
+}).prefix('/users')
+
+Route.group(() => {
+  Route.get('/permissions/:id', 'PermissionsController.show')
+  Route.get('/permissions', 'PermissionsController.index')
+  Route.post('/permissions', 'PermissionsController.store')
+  Route.delete('/permissions', 'PermissionsController.destroy')
 })
+  .prefix('/permissions')
+  .middleware('auth:api')
+  .middleware('isAdmin')
