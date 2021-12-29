2ea4a6c54824c7e3aecff85ad542de0b1e3cd30e
Alberto Jose <albertojcvs@gmail.com>
2021-12-29 10:08:06 -0300

    fix names in clientStatus(ms-clients) and Create uservalidator(bank_system)

--- Bank_Sytem/app/Validators/CreateUserValidator.ts
+++ Bank_Sytem/app/Validators/CreateUserValidator.ts
@@ -1,7 +1,7 @@
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class SaveUserValidator {
+export default class CreateUserValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
--- Ms_Clients/src/enums/ClientStatus.ts
+++ Ms_Clients/src/enums/ClientStatus.ts
@@ -1,4 +1,4 @@
 export enum ClientStatus{
     APPROVED,
-    REJECTD
+    REJECTED
 }
