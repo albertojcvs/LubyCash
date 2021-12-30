2f4834a936e371e02911186fd4577c478deb923a
Alberto Jose <albertojcvs@gmail.com>
2021-12-30 09:45:32 -0300

    add the test of get all clients wth jest

---
+++ Ms_Clients/src/tests/ReadClient.spec.ts
@@ -0,0 +1,11 @@
+import supertest from "supertest";
+import { app } from "../app";
+
+describe("Get all clients", () => {
+  test("It should be able to get all clients and their information", async () => {
+    const { text, statusCode } = await supertest(app).get("/clients");
+    console.log(text);
+
+    expect(statusCode).toBe(200);
+  });
+});
