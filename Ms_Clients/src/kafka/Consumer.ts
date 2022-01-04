b8a8b924bac76e8c74bc095fcb534fea392ae364
Alberto Jose <albertojcvs@gmail.com>
2022-01-04 14:14:22 -0300

    add the topics and their handles

--- Ms_Clients/src/kafka/Consumer.ts
+++ Ms_Clients/src/kafka/Consumer.ts
@@ -1,8 +1,17 @@
 import { Kafka, Consumer as KafkaConsumer } from "kafkajs";
+import { MailtrapMailProvider } from "../providers/implementations/mail/MailTrapMailProvider";
+import { PrismaClientRepository } from "../repositories/implementations/prismaImplementations/PrismaClientRepository";
+import { PrismaTransactionRepository } from "../repositories/implementations/prismaImplementations/PrismaTransactionRepository";
+import { CreateClientHandler } from "./handlers/CreateClient";
+import { PixTransactioHandler } from "./handlers/PixTransaction";
 
 interface IConsumerProps {
   groupId: string;
 }
+
+const clientRepository = new PrismaClientRepository();
+const transactionReposiory = new PrismaTransactionRepository();
+const mailProvider = new MailtrapMailProvider();
 
 export class Consumer {
   private consumer: KafkaConsumer;
@@ -13,7 +22,7 @@ export class Consumer {
   }
 
   public async consume() {
-    const topics = [] as const;
+    const topics = ["pix-transaction", "create-client"] as const;
 
     await Promise.all(
       topics.map((topic) => {
@@ -21,6 +30,28 @@ export class Consumer {
       })
     );
 
-    await this.consumer.run({ eachMessage: async () => {} });
+    await this.consumer.run({
+      eachMessage: async ({ topic, message }) => {
+        if (!message.value) return;
+        const data = JSON.parse(message.value.toString());
+        switch (topic) {
+          case "pix-transaction":
+            const pixTransactionHandler = new PixTransactioHandler(
+              clientRepository,
+              transactionReposiory,
+              mailProvider
+            );
+
+            await pixTransactionHandler.handle(data);
+            break;
+          case 'create-client':
+            const createClientHandler = new CreateClientHandler(clientRepository,mailProvider)
+            await createClientHandler.handle(data)
+            break;
+          default:
+            console.log("ERRORRRRRR");
+        }
+      },
+    });
   }
 }
