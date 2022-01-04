2d2f960b15401b31040ccf3141190d62f264a325
Alberto Jose <albertojcvs@gmail.com>
2022-01-04 14:33:25 -0300

    refactor some files

--- Ms_Clients/src/routes/clients/index.ts
+++ Ms_Clients/src/routes/clients/index.ts
@@ -1,23 +1,21 @@
 import { Router } from "express";
 
 import { ClientsController } from "../../controllers/clients";
-import { PrismaAccountReposiory } from "../../repositories/implementations/prismaImplementations/PrismaAccountRepository";
 import { PrismaClientRepository } from "../../repositories/implementations/prismaImplementations/PrismaClientRepository";
 import { PrismaTransactionRepository } from "../../repositories/implementations/prismaImplementations/PrismaTransactionRepository";
 
-const clientRespository = new PrismaClientRepository();
+const clientRepository = new PrismaClientRepository();
 
-const accountRepository = new PrismaAccountReposiory();
 const transactionRepository = new PrismaTransactionRepository();
 
 const clientsController = new ClientsController(
-  clientRespository,
-  accountRepository,
+  clientRepository,
   transactionRepository
 );
 const clientsRoutes = Router();
 
-clientsRoutes.get("/clients", (request,response) => clientsController.getAll(request,response));
-clientsRoutes.get("/clients/:email", (request,response) => clientsController.getByEmail(request,response));
+clientsRoutes.get("/clients", (request, response) =>
+  clientsController.getAll(request, response)
+);
 
 export default clientsRoutes;
--- Ms_Clients/src/server.ts
+++ Ms_Clients/src/server.ts
@@ -1,6 +1,11 @@
 import { app } from "./app";
+import { Consumer } from "./kafka/Consumer";
 const PORT = 3000;
 
-app.listen(PORT, () => {
+const consumer = new Consumer({ groupId: "ms_clients" });
+
+consumer.consume();
+
+app.listen(PORT, async () => {
   console.log("Server is running");
 });
