b3122f9072da5235adde23a4b6f1cafe39a0da5f
Alberto Jose <albertojcvs@gmail.com>
2022-01-06 10:20:14 -0300

    send the client and permissions data when a user make a login

--- Bank_Sytem/app/Controllers/Http/LoginController.ts
+++ Bank_Sytem/app/Controllers/Http/LoginController.ts
@@ -1,19 +1,25 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 import LoginValidator from 'App/Validators/LoginValidator'
+import axios from 'axios'
+import { Client } from 'comunication/Client'
 
 export default class LoginController {
   public async login({ request, auth }: HttpContextContract) {
-
     const { email, password } = await request.validate(LoginValidator)
 
     const token = await auth.use('api').attempt(email, password)
 
     const user = await User.query()
-      .select('id','email', 'username')
-      .where('email', email).preload('permissions').firstOrFail()
+      .select('id', 'email', 'username')
+      .where('email', email)
+      .preload('permissions')
+      .firstOrFail()
 
-    return { token, user }
+    const response = await axios.get('http://ms_clients_app_1:3000/clients')
+    const clients: Client[] = response.data
+    const client = clients.filter((client) => client.email == email)[0]
+    return { token, user, client }
   }
 
   public async logout({ auth }: HttpContextContract) {
